810180a5ab11a947a25c66c17919d35a261fc52e
kcurti <kiersten.curti@noaa.gov>
2023-09-03 16:54:45 -0400

    Model Comparison code and figures

--- Bridge.Runs/Compare_Model_Ests.R
+++ Bridge.Runs/Compare_Model_Ests.R
@@ -1,10 +1,13 @@
 ### Compare model estimates across runs
 
-rm(list=ls())
+library(tidyverse)
 
-comp.name <- 'MT2021.BridgeRuns'
+rm(list=ls())
+ls()
+
 comp.dir <- 'Bridge.Runs'
-run.list <- paste("Run",2:3,sep='')
+run.list <- paste("Run",7:9,sep='')
+fig.basename <- 'Runs7-9'
 reg <- 'north'
 
 F.yr <- tibble()
@@ -17,9 +20,34 @@ for (run in run.list)
   run.env <- new.env()
   
   load(file.path(comp.dir, run, "wham", reg, "outputs", paste(run,reg,"WHAM.Outputs.RDATA",sep='.')), envir=run.env)
-  SSB.yr <- run.env$SSB.yr %>% mutate(Run = run)
+  SSB.yr <- bind_rows(SSB.yr, run.env$SSB.yr %>% mutate(Run = run))
   F.yr <- bind_rows(F.yr, run.env$F.yr %>% mutate(Run = run))
-  run.env$Rect.yr %>% mutate(Run = run)
+  Rect.yr <- bind_rows(Rect.yr, run.env$Rect.yr %>% mutate(Run = run))
+  
+  rm(run.env)
 }
 
+
+# col.list <- viridisLite::viridis(n = length(run.list))
+
+windows()
+SSB.yr %>% ggplot(aes(Year, est)) +
+  geom_line(aes(color=Run)) +
+  geom_ribbon(aes(ymin = lo, ymax = hi, fill=Run), alpha = 0.2, linetype='blank') +
+  ylab("SSB (mt)")
+ggsave(file.path('Bridge.Runs/Comparison_Figures',paste(fig.basename,reg,'SSB.png',sep='.')),device='png')
+
+windows()
+F.yr %>% ggplot(aes(Year, est)) +
+  geom_line(aes(color=Run)) +
+  geom_ribbon(aes(ymin = lo, ymax = hi, fill=Run), alpha = 0.2, linetype='blank') +
+  ylab("Fishing mortality")
+ggsave(file.path('Bridge.Runs/Comparison_Figures',paste(fig.basename,reg,'F.png',sep='.')),device='png')
+         
+windows()
+Rect.yr %>% ggplot(aes(Year, est)) +
+  geom_line(aes(color=Run)) +
+  geom_ribbon(aes(ymin = lo, ymax = hi, fill=Run), alpha = 0.2, linetype='blank') +
+  ylab("Recruitment")
+ggsave(file.path('Bridge.Runs/Comparison_Figures',paste(fig.basename,reg,'Rect.png',sep='.')),device='png')
 
