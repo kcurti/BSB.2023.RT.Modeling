c1b6416138829ab4085fda6e361c2f6aa3ec9a9e
timjmiller <timothy.j.miller@noaa.gov>
2023-09-08 21:58:47 -0400

    Run 9 done

--- 2023.RT.Runs/Run9/fit_run.R
+++ 2023.RT.Runs/Run9/fit_run.R
@@ -45,7 +45,7 @@ move$use_prior[1,1,1,1] <- 1
 move$use_prior[1,1,2,1] <- 1
 move$prior_sigma <- array(0, dim = c(2,length(seasons),2,1))
 move$prior_sigma[1,1,1,1] <- 0.21097
-move$prior_sigma[1,1,1,2] <- 0.37209
+move$prior_sigma[1,1,2,1] <- 0.37209
 
 sel <- list(model = rep(c("logistic","age-specific", "logistic",
 	"age-specific"),
@@ -77,17 +77,64 @@ sel$fix_pars <- c(rep(list(NULL),4), list(2:8), rep(list(NULL),3), list(
 temp <- prepare_wham_input(asap, selectivity = sel, NAA_re = NAA_re, basic_info = basic_info, move = move, age_comp = "logistic-normal-miss0")
 temp$fleet_names = paste0(rep(c("North_", "South_"),each = 2), temp$fleet_names)
 temp$index_names = paste0(rep(c("North_", "South_"),c(3,3)), temp$index_names)
-est_mu_map <- temp$map$trans_mu #try to estimate in a second phase
-temp$map$trans_mu <- factor(rep(NA,length(temp$par$trans_mu)))
-temp$par <- tfit2$parList
-tfit3 <- fit_wham(temp, do.retro=F, do.osa=F, do.sdrep =T)#doesn't converge. CV for 2+ north stock is large ~0.8
+Run8 <- readRDS(here("2023.RT.Runs","Run8","fit.RDS"))
+parnms <- sort(names(Run8$parList))
+#t(sapply(parnms, function(x) c(length(Run7$parList[[x]]), length(temp$par[[x]]))))
+parnms_use <- grep("mu", parnms, invert = T, value = T) #all but those for movement
+temp$par[parnms_use] <- Run8$parList[parnms_use]
+# est_mu_map <- temp$map$trans_mu #try to estimate in a second phase
+# temp$map$trans_mu <- factor(rep(NA,length(temp$par$trans_mu)))
+x <- temp$par$log_NAA_sigma
+x[] <- as.integer(temp$map$log_NAA_sigma)
+x[1,-1] <- NA
+temp$map$log_NAA_sigma <- factor(x) #don't estimates sig for north 2+
+temp$par$log_index_sig_scale[] <-0 #the large CVs for the RecCPA indices are problematic
+tfit <- fit_wham(temp, do.retro=F, do.osa=F, do.sdrep =T)
+#age 5 in selblock 10 is hitting bound
 
+sel <- list(model = rep(c("logistic","age-specific", "logistic",
+	"age-specific"),
+	c(4,1,3,
+		6)))
+sel$initial_pars <- c(rep(list(c(5,1)),4), list(rep(c(0.5,1),c(1,7))), rep(list(c(5,1)),3), list( 
+	### begin north
+	c(rep(c(0.5,1,0.5), c(2,4,2))), #rec cpa age-specfic
+	c(rep(c(0.5,1,0.5), c(2,3,3))), #VAST spring age-specfic
+	c(rep(c(0.5,1,0.5), c(2,1,5))),#) #VAST fall age-specfic
+	####end north, begin south
+	c(rep(c(0.5,1,0.5,1), c(2,1,4,1))), #rec cpa age-specific
+	c(rep(c(0.5,1,0.5), c(1,1,6))), #VAST spring age-specific
+	c(rep(c(0.5,1,0.5), c(1,1,6)))) #VAST fall age-specific
+)
+sel$fix_pars <- c(rep(list(NULL),4), list(2:8), rep(list(NULL),3), list(
+	###begin north
+  3:6, #rec cpa
+  3:5, #VAST spring
+  3, #VAST fall
+	####end north, begin south
+  c(3,8), #rec cpa
+  2, #VAST spring
+  2) #VAST fall
+)
+
+temp <- prepare_wham_input(asap, selectivity = sel, NAA_re = NAA_re, basic_info = basic_info, move = move, age_comp = "logistic-normal-miss0")
+temp$fleet_names = paste0(rep(c("North_", "South_"),each = 2), temp$fleet_names)
+temp$index_names = paste0(rep(c("North_", "South_"),c(3,3)), temp$index_names)
+temp$par <- tfit$parList
+temp$par$logit_selpars[10,5] <- Inf
+temp$par$log_index_sig_scale[] <-0 #the large CVs for the RecCPA indices are problematic
+x <- temp$par$log_NAA_sigma
+x[] <- as.integer(temp$map$log_NAA_sigma)
+x[1,-1] <- NA
+temp$map$log_NAA_sigma <- factor(x) #don't estimates sig for north 2+
+tfit2 <- fit_wham(temp, do.fit=F)
+tfit2 <- fit_wham(temp, do.retro=T, do.osa=T,do.sdrep=T,do.brps=T)
 
 input <- tfit2$input
 input$par <- tfit2$parList
 fit <- fit_wham(input, do.retro=T, do.osa=T, do.sdrep =T, do.brps=T)
 mohns_rho(fit)
-setwd(here("2023.RT.Runs","Run8"))
+setwd(here("2023.RT.Runs","Run9"))
 saveRDS(fit,"fit.RDS")
 plot_wham_output(fit)
 setwd(here())
