46b3a8054648c512fafc6a4912612c5a8156a27b
Emily Liljestrand <emily.liljestrand@noaa.gov>
2024-01-22 12:34:47 -0500

    Output and Development of Run37
    Added code and results for model run 37 which decomposes VAST indices into individual state/fed indices

--- 2023.RT.Runs/Run37/fit_run.R
+++ 2023.RT.Runs/Run37/fit_run.R
@@ -1174,7 +1174,7 @@ sel$initial_pars <- list(
   c(rep(c(0.5,1), c(3,5))), #north neamap
   c(rep(c(0.5,1), c(2,6))), #north MA
   c(rep(c(0.5,1), c(2,6))), #north RI
-  c(rep(c(0.5,1), c(2,6))), #north CT
+  c(rep(c(0.5,1), c(2,6))), #north CT (Long Island)
   c(rep(c(1,0), c(1,7))), #north NY
   c(rep(c(0.5,1,1),c(1,1,6))), #north rec cpa
   c(rep(c(0.5,1), c(1,7))), #north Bigelow
@@ -1184,7 +1184,7 @@ sel$initial_pars <- list(
   c(rep(c(1,0), c(1,7))), #south DE
   c(rep(c(1,0), c(1,7))), #south MD
   c(rep(c(1,0), c(1,7))), #south VIMS
-  c(rep(c(0.5,1), c(2,6))), #south winter
+  c(rep(c(0.5,1), c(2,6))), #south winter bottom trawl
   c(rep(c(0.5,1,1),c(2,4,2))), #south rec cpa (like bridge 7 but replaced with values in run 34)
   c(rep(c(0.5,1), c(1,7))) #south bigelow
 )
@@ -1238,13 +1238,317 @@ x <- array(as.integer(temp$map$trans_NAA_rho), dim = dim(temp$par$trans_NAA_rho)
 x[1,2,] <- NA #don't estimate AR1 cor parameters for north population in the south.
 temp$map$trans_NAA_rho <- factor(x)
 
-# tfit7 <- fit_wham(temp, do.sdrep = F, do.osa = F, do.retro = F)
-# saveandplot(tfit7,"tfit7")
-# fit7 <- fit_wham(temp, do.sdrep = T, do.osa = T, do.retro = T, do.brps = T)
-# saveandplot(fit7,"fit7")
-#End Model 7 ---------------------------
+tfit8 <- fit_wham(temp, do.sdrep = F, do.osa = F, do.retro = F)
+saveandplot(tfit8,"tfit8")
 
-#Leftover Code from Run37 ---------------------------
+fit8 <- fit_wham(temp, do.sdrep = T, do.osa = T, do.retro = T, do.brps = T)
+# saveandplot(fit8,"fit8")
+#End Model 8 ---------------------------
+
+#Model 9 ---------------------------
+#Same as model 8 but with different age composition likelihoods 
+#With: environmental covariates, movement, random effects
+#Catch still has random effects on selectivity
+# for(i in c(1:6,8:15,17)) asap_alt$dat$IAA_mats[[i]] <- asap$dat$IAA_mats[[i]]
+# north_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp-nmab.csv"))
+# south_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp-smab.csv"))
+north_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp_nmab_1959-2022.csv"))
+south_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp_smab_1959-2022.csv"))
+
+ecov <- list(label = c("North_BT","South_BT"))
+ecov$mean <- cbind(north_bt[,'mean'], south_bt[,'mean'])
+ecov$logsigma <- log(cbind(north_bt[,'se'], south_bt[,'se']))
+ecov$year <- north_bt[,'year']
+ecov$use_obs <- matrix(1, NROW(ecov$mean),NCOL(ecov$mean))
+#ecov$lag <- 1
+ecov$process_model <- "ar1"
+ecov$process_mean_vals <- apply(ecov$mean, 2, mean)
+ecov$recruitment_how <- matrix(c("controlling-lag-0-linear","none","none","none"), 2,2)
+
+NAA_re = list(sigma = list("rec+1","rec+1"), cor = list("2dar1","2dar1"), N1_model = rep("equilibrium",2))
+basic_info <- list(region_names = c("North", "South"), stock_names = paste0("BSB_", c("North", "South"))) #, NAA_where = array(1, dim = c(2,2,6)))
+temp <- prepare_wham_input(asap_alt, ecov = ecov, NAA_re = NAA_re, basic_info = basic_info)
+# temp <- prepare_wham_input(asap_alt, NAA_re = NAA_re, basic_info = basic_info)
+
+
+#11 seasons each 1 month long except a 2 month interval in the model (June,July)
+seasons = c(rep(1,5),2,rep(1,5))/12
+basic_info$fracyr_seasons <- seasons
+#each age other than 1 (recruitment) for north stock can be in either region on Jan 1 
+basic_info$NAA_where <- array(1, dim = c(2,2,8))
+basic_info$NAA_where[1,2,1] = 0 #stock 1, age 1 can't be in region 2 
+basic_info$NAA_where[2,1,] = 0 #stock 2, any age can't be in region 1 (stock 2 doesn't move) 
+
+#average recruitment over years 2000+ for SSB40 BRPs
+basic_info$XSPR_R_avg_yrs <- which(temp$years>1999)
+basic_info$XSPR_R_opt <- 2 #use average of recruitments (random effects), not expected/predicted given last time step
+
+move = list(stock_move = c(TRUE,FALSE), separable = TRUE) #north moves, south doesn't
+
+move$must_move = array(0,dim = c(2,length(seasons),2))	
+
+#if north stock in region 2 (south) must move back to region 1 (north) at the end of interval 5 right before spawning
+move$must_move[1,5,2] <- 1 
+move$can_move = array(0, dim = c(2,length(seasons),2,2))
+move$can_move[1,c(1:4),2,1] <- 1 #only north stock can move and in seasons prior to spawning and after spawning
+move$can_move[1,c(7:11),1,2] <- 1 #only north stock can move and in seasons prior to spawning and after spawning
+move$can_move[1,5,2,] <- 1 #north stock can (and must) move in last season prior to spawning back to north 
+
+mus <- array(0, dim = c(2,length(seasons),2,1))
+mus[1,1:11,1,1] <- 0.02214863 #see here("2023.RT.Runs","transform_SS_move_rates.R") for how these numbers are derived.
+mus[1,1:11,2,1] <- 0.3130358
+move$mean_vals <- mus 
+
+move$mean_model = matrix("stock_constant", 2,1)
+
+#prior distribution on movement parameters 
+move$use_prior <- array(0, dim = c(2,length(seasons),2,1))
+move$use_prior[1,1,1,1] <- 1
+move$use_prior[1,1,2,1] <- 1
+move$prior_sigma <- array(0, dim = c(2,length(seasons),2,1))
+move$prior_sigma[1,1,1,1] <- 0.2
+move$prior_sigma[1,1,2,1] <- 0.2
+
+sel <- list(model = rep(c("age-specific","logistic","age-specific","age-specific"),
+                        c(2,2,4,8+9)))
+sel$initial_pars <- list(
+  rep(c(0.5,1),c(3,5)), #north comm
+  rep(c(0.5,1),c(6,2)), #north rec
+  c(5,1), #south comm
+  c(5,1),	#south rec
+  rep(0.5,8), #not used
+  rep(0.5,8), #not used 
+  rep(0.5,8), #not used
+  rep(0.5,8), #not used
+  c(rep(c(0.5,1), c(1,7))), #north spring Alb
+  c(rep(c(0.5,1), c(3,5))), #north neamap
+  c(rep(c(0.5,1), c(2,6))), #north MA
+  c(rep(c(0.5,1), c(2,6))), #north RI
+  c(rep(c(0.5,1), c(2,6))), #north CT (Long Island)
+  c(rep(c(1,0), c(1,7))), #north NY
+  c(rep(c(0.5,1,1),c(1,1,6))), #north rec cpa
+  c(rep(c(0.5,1), c(1,7))), #north Bigelow
+  c(rep(c(0.5,1), c(3,5))), #south spring alb
+  c(rep(c(1,0), c(1,7))), #south neamap
+  c(rep(c(0.5,1), c(1,7))), #south NJ
+  c(rep(c(1,0), c(1,7))), #south DE
+  c(rep(c(1,0), c(1,7))), #south MD
+  c(rep(c(1,0), c(1,7))), #south VIMS
+  c(rep(c(0.5,1), c(2,6))), #south winter bottom trawl
+  c(rep(c(0.5,1,1),c(2,4,2))), #south rec cpa (like bridge 7 but replaced with values in run 34)
+  c(rep(c(0.5,1), c(1,7))) #south bigelow
+)
+sel$fix_pars <- list(
+  4:8, #north comm
+  7:8, #north rec
+  NULL, #south comm
+  NULL, #south rec
+  1:8, #not used
+  1:8, #not used
+  1:8, #not used
+  1:8, #not used
+  2:8, #north spring alb
+  4:8, #north neamap
+  3:8, #north MA
+  3:8, #north RI
+  3:8, #north CT
+  1:8, #north NY
+  2:8, #north rec cpa
+  2:8, #north bigelow
+  4:8, #south spring alb
+  1:8, #south neamap
+  2:8, #south NJ
+  1:8, #south DE
+  1:8, #south MD
+  1:8, #south VIMS
+  3:8, #south winter
+  3:8, #south rec cpa
+  2:8 #south bigelow
+)
+sel$re <- rep(c("2dar1","2dar1","none","none","none"), c(1,1,2+4+6,1,10))
+#ar1_y on north rec cpa
+temp <- prepare_wham_input(asap_alt, selectivity = sel, NAA_re = NAA_re, basic_info = basic_info, move = move, ecov = ecov,
+                           age_comp = list(
+                             fleets = c("dir-mult","logistic-normal-miss0","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0"), 
+                             indices = rep(c("dir-mult","logistic-normal-miss0","dir-mult","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0"),c(6,1,1,7,1,1))))
+# indices = rep(c("dir-mult","logistic-normal-miss0","dir-mult","logistic-normal-ar1-miss0","dir-mult"),c(6,1,8,1,1))))
+temp$fleet_names = paste0(rep(c("North_", "South_"),each = 2), temp$fleet_names)
+temp$index_names = paste0(rep(c("North_", "South_"),c(8,9)), temp$index_names)
+temp$map$trans_mu <- factor(rep(NA,length(temp$par$trans_mu)))
+temp$data$selblock_pointer_fleets[] <- rep(1:4, each = length(temp$years))
+temp <- wham:::set_selectivity(temp,sel)
+#temp$data$agg_index_sigma[,c(1,3)] <- 5*temp$data$agg_index_sigma[,c(1,3)]
+#estimate log_index_sig_scale for the rec CPA indices
+temp$par$log_index_sig_scale[c(7,16)] <- log(5)
+temp$map$log_index_sig_scale  <- factor(c(NA,NA,NA,NA,NA,NA,1,NA,NA,NA,NA,NA,NA,NA,NA,2,NA))
+x <- array(as.integer(temp$map$log_NAA_sigma), dim = dim(temp$par$log_NAA_sigma))
+x[1,2,2:8] <- NA #allow sigmas to be different for the two regions for north pop
+temp$map$log_NAA_sigma <- factor(x)
+temp$par$log_NAA_sigma[1,2,2:8] <- log(0.05) #fix sigmas to be very low (~SCAA) for north population in the south
+x <- array(as.integer(temp$map$trans_NAA_rho), dim = dim(temp$par$trans_NAA_rho))
+x[1,2,] <- NA #don't estimate AR1 cor parameters for north population in the south.
+temp$map$trans_NAA_rho <- factor(x)
+
+tfit9 <- fit_wham(temp, do.sdrep = F, do.osa = F, do.retro = F)
+saveandplot(tfit9,"tfit9")
+
+fit9 <- fit_wham(temp, do.sdrep = T, do.osa = T, do.retro = T, do.brps = T)
+# saveandplot(fit9,"fit9")
+#End Model 9 ---------------------------
+
+#Model 10 ---------------------------
+#Same as model 8 but with different age composition likelihoods 
+#With: environmental covariates, movement, random effects
+#Catch still has random effects on selectivity
+# for(i in c(1:6,8:15,17)) asap_alt$dat$IAA_mats[[i]] <- asap$dat$IAA_mats[[i]]
+# north_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp-nmab.csv"))
+# south_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp-smab.csv"))
+north_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp_nmab_1959-2022.csv"))
+south_bt <- read.csv(here("2023.RT.Runs","Run33","bsb_bt_temp_smab_1959-2022.csv"))
+
+ecov <- list(label = c("North_BT","South_BT"))
+ecov$mean <- cbind(north_bt[,'mean'], south_bt[,'mean'])
+ecov$logsigma <- log(cbind(north_bt[,'se'], south_bt[,'se']))
+ecov$year <- north_bt[,'year']
+ecov$use_obs <- matrix(1, NROW(ecov$mean),NCOL(ecov$mean))
+#ecov$lag <- 1
+ecov$process_model <- "ar1"
+ecov$process_mean_vals <- apply(ecov$mean, 2, mean)
+ecov$recruitment_how <- matrix(c("controlling-lag-0-linear","none","none","none"), 2,2)
+
+NAA_re = list(sigma = list("rec+1","rec+1"), cor = list("2dar1","2dar1"), N1_model = rep("equilibrium",2))
+basic_info <- list(region_names = c("North", "South"), stock_names = paste0("BSB_", c("North", "South"))) #, NAA_where = array(1, dim = c(2,2,6)))
+temp <- prepare_wham_input(asap_alt, ecov = ecov, NAA_re = NAA_re, basic_info = basic_info)
+# temp <- prepare_wham_input(asap_alt, NAA_re = NAA_re, basic_info = basic_info)
+
+
+#11 seasons each 1 month long except a 2 month interval in the model (June,July)
+seasons = c(rep(1,5),2,rep(1,5))/12
+basic_info$fracyr_seasons <- seasons
+#each age other than 1 (recruitment) for north stock can be in either region on Jan 1 
+basic_info$NAA_where <- array(1, dim = c(2,2,8))
+basic_info$NAA_where[1,2,1] = 0 #stock 1, age 1 can't be in region 2 
+basic_info$NAA_where[2,1,] = 0 #stock 2, any age can't be in region 1 (stock 2 doesn't move) 
+
+#average recruitment over years 2000+ for SSB40 BRPs
+basic_info$XSPR_R_avg_yrs <- which(temp$years>1999)
+basic_info$XSPR_R_opt <- 2 #use average of recruitments (random effects), not expected/predicted given last time step
+
+move = list(stock_move = c(TRUE,FALSE), separable = TRUE) #north moves, south doesn't
+
+move$must_move = array(0,dim = c(2,length(seasons),2))	
+
+#if north stock in region 2 (south) must move back to region 1 (north) at the end of interval 5 right before spawning
+move$must_move[1,5,2] <- 1 
+move$can_move = array(0, dim = c(2,length(seasons),2,2))
+move$can_move[1,c(1:4),2,1] <- 1 #only north stock can move and in seasons prior to spawning and after spawning
+move$can_move[1,c(7:11),1,2] <- 1 #only north stock can move and in seasons prior to spawning and after spawning
+move$can_move[1,5,2,] <- 1 #north stock can (and must) move in last season prior to spawning back to north 
+
+mus <- array(0, dim = c(2,length(seasons),2,1))
+mus[1,1:11,1,1] <- 0.02214863 #see here("2023.RT.Runs","transform_SS_move_rates.R") for how these numbers are derived.
+mus[1,1:11,2,1] <- 0.3130358
+move$mean_vals <- mus 
+
+move$mean_model = matrix("stock_constant", 2,1)
+
+#prior distribution on movement parameters 
+move$use_prior <- array(0, dim = c(2,length(seasons),2,1))
+move$use_prior[1,1,1,1] <- 1
+move$use_prior[1,1,2,1] <- 1
+move$prior_sigma <- array(0, dim = c(2,length(seasons),2,1))
+move$prior_sigma[1,1,1,1] <- 0.2
+move$prior_sigma[1,1,2,1] <- 0.2
+
+sel <- list(model = rep(c("age-specific","logistic","age-specific","age-specific"),
+                        c(2,2,4,8+9)))
+sel$initial_pars <- list(
+  rep(c(0.5,1),c(3,5)), #north comm
+  rep(c(0.5,1),c(6,2)), #north rec
+  c(5,1), #south comm
+  c(5,1),	#south rec
+  rep(0.5,8), #not used
+  rep(0.5,8), #not used 
+  rep(0.5,8), #not used
+  rep(0.5,8), #not used
+  c(rep(c(0.5,1), c(1,7))), #north spring Alb
+  c(rep(c(0.5,1), c(3,5))), #north neamap
+  c(rep(c(0.5,1), c(2,6))), #north MA
+  c(rep(c(0.5,1), c(2,6))), #north RI
+  c(rep(c(0.5,1), c(2,6))), #north CT (Long Island)
+  c(rep(c(1,0), c(1,7))), #north NY
+  c(rep(c(0.5,1,1),c(1,1,6))), #north rec cpa
+  c(rep(c(0.5,1), c(1,7))), #north Bigelow
+  c(rep(c(0.5,1), c(3,5))), #south spring alb
+  c(rep(c(1,0), c(1,7))), #south neamap
+  c(rep(c(0.5,1), c(1,7))), #south NJ
+  c(rep(c(1,0), c(1,7))), #south DE
+  c(rep(c(1,0), c(1,7))), #south MD
+  c(rep(c(1,0), c(1,7))), #south VIMS
+  c(rep(c(0.5,1), c(2,6))), #south winter bottom trawl
+  c(rep(c(0.5,1,1),c(2,4,2))), #south rec cpa (like bridge 7 but replaced with values in run 34)
+  c(rep(c(0.5,1), c(1,7))) #south bigelow
+)
+sel$fix_pars <- list(
+  4:8, #north comm
+  7:8, #north rec
+  NULL, #south comm
+  NULL, #south rec
+  1:8, #not used
+  1:8, #not used
+  1:8, #not used
+  1:8, #not used
+  2:8, #north spring alb
+  4:8, #north neamap
+  3:8, #north MA
+  3:8, #north RI
+  3:8, #north CT
+  1:8, #north NY
+  2:8, #north rec cpa
+  2:8, #north bigelow
+  4:8, #south spring alb
+  1:8, #south neamap
+  2:8, #south NJ
+  1:8, #south DE
+  1:8, #south MD
+  1:8, #south VIMS
+  3:8, #south winter
+  3:8, #south rec cpa
+  2:8 #south bigelow
+)
+# sel$re <- rep(c("2dar1","2dar1","none","none","none"), c(1,1,2+4+6,1,10))
+sel$re <- rep(c("2dar1","2dar1","none","ar1_y","none"), c(1,1,2+4+6,1,10))
+#ar1_y on north rec cpa
+temp <- prepare_wham_input(asap_alt, selectivity = sel, NAA_re = NAA_re, basic_info = basic_info, move = move, ecov = ecov,
+                           age_comp = list(
+                             fleets = c("dir-mult","logistic-normal-miss0","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0"), 
+                             indices = rep(c("dir-mult","logistic-normal-miss0","dir-mult","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0","logistic-normal-ar1-miss0"),c(6,1,1,7,1,1))))
+# indices = rep(c("dir-mult","logistic-normal-miss0","dir-mult","logistic-normal-ar1-miss0","dir-mult"),c(6,1,8,1,1))))
+temp$fleet_names = paste0(rep(c("North_", "South_"),each = 2), temp$fleet_names)
+temp$index_names = paste0(rep(c("North_", "South_"),c(8,9)), temp$index_names)
+temp$map$trans_mu <- factor(rep(NA,length(temp$par$trans_mu)))
+temp$data$selblock_pointer_fleets[] <- rep(1:4, each = length(temp$years))
+temp <- wham:::set_selectivity(temp,sel)
+#temp$data$agg_index_sigma[,c(1,3)] <- 5*temp$data$agg_index_sigma[,c(1,3)]
+#estimate log_index_sig_scale for the rec CPA indices
+temp$par$log_index_sig_scale[c(7,16)] <- log(5)
+temp$map$log_index_sig_scale  <- factor(c(NA,NA,NA,NA,NA,NA,1,NA,NA,NA,NA,NA,NA,NA,NA,2,NA))
+x <- array(as.integer(temp$map$log_NAA_sigma), dim = dim(temp$par$log_NAA_sigma))
+x[1,2,2:8] <- NA #allow sigmas to be different for the two regions for north pop
+temp$map$log_NAA_sigma <- factor(x)
+temp$par$log_NAA_sigma[1,2,2:8] <- log(0.05) #fix sigmas to be very low (~SCAA) for north population in the south
+x <- array(as.integer(temp$map$trans_NAA_rho), dim = dim(temp$par$trans_NAA_rho))
+x[1,2,] <- NA #don't estimate AR1 cor parameters for north population in the south.
+temp$map$trans_NAA_rho <- factor(x)
+
+tfit10 <- fit_wham(temp, do.sdrep = F, do.osa = F, do.retro = F)
+saveandplot(tfit10,"tfit10")
+
+fit10 <- fit_wham(temp, do.sdrep = T, do.osa = T, do.retro = T, do.brps = T)
+# saveandplot(fit10,"fit10")
+#End Model 9 ---------------------------
+
+#Leftover Code from Run34 ---------------------------
 # x <- readRDS(here("2023.RT.Runs",this_run, "fit.RDS"))
 # source(here("2023.RT.Runs", "kobe.plot.R"))
 # kobe.plot(x, status.years=NULL, static = FALSE, single.plot = TRUE)
